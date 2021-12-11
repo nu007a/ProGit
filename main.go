582c4f8ed58edc9fc8c17a4b9d732ea1cf1164bf
michaelni <nu007a@126.com>
2021-12-11 14:10:56 +0800

    add feat

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type A struct {
+	id int
+}
 
 func main() {
-	fname:= "Edward"
-	lname:= "Scissorhands"
-	fmt.Printf("Hello Mr. %s %s",fname,lname)
+	channel := make(chan A, 5)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for a := range channel {
+			wg.Add(1)
+			item := a
+			go func() {
+				defer wg.Done()
+				fmt.Println(item.id)
+			}()
+		}
+
+	}()
+
+	for i := 0; i < 10; i++ {
+		channel <- A{id: i}
+	}
+	close(channel)
+
+	wg.Wait()
 }
